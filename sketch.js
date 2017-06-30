d898446def8953c8c381467f5709ecb825e0007d
Marcin <you@example.com>
2017-06-30 18:23:29 +0200

    Added first paddle controlled by mouse

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ var ball = {
     
     xPosition : 200,
     yPosition : 400,
-    xSpeed : 3,
-    ySpeed : 3,
+    xSpeed : 5,
+    ySpeed : 5,
     
     move : function()
     {
@@ -19,6 +19,11 @@ var ball = {
             {
                 this.xSpeed*=-1;
             }
+         if(this.xPosition==paddle1.xPosition && (this.yPosition<=paddle1.yPosition+40 && this.yPosition>=paddle1.yPosition))
+             {
+                 this.xSpeed*=-1;
+             }
+        
     },
     
     display : function()
@@ -27,17 +32,37 @@ var ball = {
     }
 };
 
+var paddle1 = {
+    xPosition : 50,
+    yPosition : 400,
+    
+    display : function()
+    {
+        line(this.xPosition,this.yPosition,this.xPosition,this.yPosition+40);
+    },
+    
+    move : function()
+    {
+        this.yPosition=mouseY;
+        
+        if(mouseY>height-40)
+            {
+               this.yPosition=height-40;
+            }
+    }
+};
 
 function setup()
 {
     createCanvas(800,600);
+    
 }
 
 function draw()
 {
     background(0);
     stroke(255);
-    
+    strokeWeight(1);
   for(var i =0; i < height; i+=20)
       {
           line(width/2,i+10,width/2,i+20);
@@ -45,5 +70,13 @@ function draw()
     
     ball.display();
     ball.move();
+    
+    strokeWeight(4);
+    
+    paddle1.display();
+    paddle1.move();
+    
+    
+
     
 }
