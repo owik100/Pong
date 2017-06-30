c8c33ee257d3a8558b85c52f664adf282a616b00
Marcin <you@example.com>
2017-06-30 22:16:17 +0200

    Added second paddle controlled by computer

--- sketch.js
+++ sketch.js
@@ -9,39 +9,79 @@ var ball = {
     {
         this.xPosition+=this.xSpeed;
         this.yPosition+=this.ySpeed;
-        
-        if(this.yPosition>height || this.yPosition<0)
-            {
-              this.ySpeed*=-1;
-            }
-        
-        if(this.xPosition>width || this.xPosition <0)
-            {
-                this.xSpeed*=-1;
-            }
-         if(this.xPosition==paddle1.xPosition && (this.yPosition<=paddle1.yPosition+40 && this.yPosition>=paddle1.yPosition))
-             {
-                 this.xSpeed*=-1;
-             }
-        
     },
     
     display : function()
     {
-        rect(this.xPosition,this.yPosition,4,4);
+        rect(this.xPosition,this.yPosition,1,1);
+    },
+    
+    bounce : function()
+    {
+         if(this.yPosition>height || this.yPosition<0) //Odbicie od góry i dołu
+            {
+              this.ySpeed*=-1;
+            }
+        
+        if(this.xPosition>width) //Punkt dla gracza
+            {
+                pointCounter.playerPoints++;
+                ball.xPosition=400;
+                ball.yPosition=random(0,height)
+            }
+        
+        if(this.xPosition <0) //Punkt dla gracza
+            {
+                pointCounter.computerPoints++;
+                ball.xPosition=400;
+                ball.yPosition=random(0,height)
+            }
+         if(this.xPosition==paddle1.xPosition && (this.yPosition<=paddle1.yPosition+40 && this.yPosition>=paddle1.yPosition)) //Odbijanie od 1 paletki
+             {
+                 this.xSpeed*=-1;
+             }
+        if(this.xPosition==paddle2.xPosition && (this.yPosition<=paddle2.yPosition+40 && this.yPosition>=paddle2.yPosition)) //Odbijanie od 2 paletki
+             {
+                 this.xSpeed*=-1;
+             }
     }
 };
 
 var paddle1 = {
     xPosition : 50,
-    yPosition : 400,
+    yPosition : 300,
+    speed : 4,
     
     display : function()
     {
         line(this.xPosition,this.yPosition,this.xPosition,this.yPosition+40);
     },
     
-    move : function()
+     move : function()
+    {
+        if(ball.yPosition>this.yPosition)
+            {
+                this.yPosition+=this.speed;
+            }
+        else if (ball.yPosition<this.yPosition)
+            {
+                this.yPosition-=this.speed;
+            }
+    }
+};
+
+var paddle2 = {
+    
+    xPosition : 750,
+    yPosition : 300,
+    
+    display : function()
+    {
+        line(this.xPosition,this.yPosition,this.xPosition,this.yPosition+40);
+    },
+   
+    
+     move : function()
     {
         this.yPosition=mouseY;
         
@@ -50,12 +90,26 @@ var paddle1 = {
                this.yPosition=height-40;
             }
     }
+    
+    
+};
+
+var pointCounter = {
+playerPoints : 0,
+computerPoints: 0,
+    
+showPoint : function()
+    {
+        textSize(72);
+        fill(255);
+        text(this.playerPoints,300,100);
+        text(this.computerPoints,472,100);
+    }
 };
 
 function setup()
 {
     createCanvas(800,600);
-    
 }
 
 function draw()
@@ -68,13 +122,20 @@ function draw()
           line(width/2,i+10,width/2,i+20);
       }
     
+    strokeWeight(4);
+    
     ball.display();
     ball.move();
-    
-    strokeWeight(4);
+    ball.bounce();
     
     paddle1.display();
     paddle1.move();
+    
+    paddle2.display();
+    paddle2.move();
+    
+   
+    pointCounter.showPoint();
     
     
 
